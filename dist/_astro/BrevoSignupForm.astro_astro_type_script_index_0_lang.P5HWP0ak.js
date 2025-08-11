8e95efe5af072c4846ca79d899fdf1c832f5d381
James <james@odda.dev>
2025-08-12 09:35:08 +1000

    fixed typescript errors

---
+++ dist/_astro/BrevoSignupForm.astro_astro_type_script_index_0_lang.P5HWP0ak.js
@@ -0,0 +1,1 @@
+window.addEventListener("load",function(){typeof window.grecaptcha<"u"&&window.grecaptcha.ready(function(){console.log("reCAPTCHA ready")})});
---
+++ dist/_astro/hoisted.CbnwEjfx.js
@@ -0,0 +1,1 @@
+import"./hoisted.BJYPsWGg.js";import"./BrevoSignupForm.astro_astro_type_script_index_0_lang.P5HWP0ak.js";import"https://www.google.com/recaptcha/api.js?render=6LfFtzkcAAAAAM2dwJW33LNdu0KWm9cUGPAkRCqk";
